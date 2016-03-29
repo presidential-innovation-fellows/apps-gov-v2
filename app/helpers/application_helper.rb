5ce037e0202540ba99ce2646a4512ce7c454cb97
Luke Keller <lukeekeller@gmail.com>
2016-03-29 10:06:56 -0400

    Homepage Styles and Content
    Adds the homepage hero banner and styles.
    
    * Add Homepage Banner
    * Add Homepage styles
    * Modify body class via helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def static_page_body_class
+    if controller_name == "pages"
+      "pages pages-#{page_slug}"
+    end
+  end
+
+  private
+
+  def page_slug
+    params[:id].gsub("/", "-")
+  end
 end
