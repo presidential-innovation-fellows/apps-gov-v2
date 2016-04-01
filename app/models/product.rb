4671cac196fae658b5999b21ecf3af0992c8b6e7
Luke Keller <lukeekeller@gmail.com>
2016-04-01 11:51:14 -0400

    Add Sales POC Validation to Product
    Adds sales poc validation to product.
    
    * Add sales poc validation to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates \
     :long_description,
     :name, :slug,
+    :sales_poc,
     :short_description,
     :url,
     presence: true
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 describe Product do
   it { should validate_presence_of :long_description }
   it { should validate_presence_of :name }
+  it { should validate_presence_of :sales_poc }
   it { should validate_presence_of :short_description }
   it { should validate_presence_of :slug }
   it { should validate_presence_of :url }
