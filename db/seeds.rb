1b29bebc4ed802e1d6f6c37d1a0488aed8983766
Luke Keller <lukeekeller@gmail.com>
2016-06-14 16:19:17 -0400

    Seed Product ATO Status on Creation
    Creates an ATO Status for a Product depending on the presence of certain keys.
    
    * Seed Product ATO Status after Product creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,4 +33,37 @@ Dir["db/product-seeds/*.json"].each do |product_json_file|
       Customer.create!(agency: agency, product: product)
     end
   end
+
+  if product_hash["ato_agency"]
+    ato_type = AtoType.find_by(slug: "agency-approval")
+    AtoStatus.create!(ato_type: ato_type, product: product)
+  end
+
+  if product_hash["fedramp_agency"]
+    ato_type = AtoType.find_by(slug: "fedramp-agency")
+    AtoStatus.create!(
+      ato_type: ato_type,
+      product: product,
+      url: product_hash["fedramp_agency"]
+    )
+  end
+
+  if product_hash["fedramp_inprocess_agency"]
+    ato_type = AtoType.find_by(slug: "fedramp-agency")
+    AtoStatus.create!(
+      ato_type: ato_type,
+      pending: true,
+      product: product,
+      url: product_hash["fedramp_inprocess_agency"]
+    )
+  end
+
+  if product_hash["fedramp_jab"]
+    ato_type = AtoType.find_by(slug: "fedramp-jab")
+    AtoStatus.create!(
+      ato_type: ato_type,
+      product: product,
+      url: product_hash["fedramp_jab"]
+    )
+  end
 end
