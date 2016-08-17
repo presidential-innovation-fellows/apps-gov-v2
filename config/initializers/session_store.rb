561aaf9986792e7b39fc80fd071d3bc0910383cc
Luke Keller <lukeekeller@gmail.com>
2016-08-17 10:22:05 -0400

    Force HTTPS for use of cookie_store
    Force HTTPS for use of cookie_store in production.
    
    * Set secure option to true for cookie_store when in Production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_apps-gov-v2_session'
+Rails.application.config.session_store \
+  :cookie_store,
+  key: "_apps-gov-v2_session",
+  secure: Rails.env.production?
